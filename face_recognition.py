b6fe32960473594e5304bce6c522e225dd7f38f2
Antoni Podebski <antekyt123@gmail.com>
2024-06-23 21:39:21 +0200

    fix camera input

--- face_recognition.py
+++ face_recognition.py
@@ -11,13 +11,18 @@ model = tf.keras.models.load_model("face_recognition.keras")
 
 
 def evaluate_photo(photo):
-    image = Image.open(photo)
-    image_np = np.array(image)
-    input_image = image_np.reshape((1, 48, 48, 1)).astype("float32") / 255.0
-    result = model.predict(input_image)
-    pred_labels = np.argmax(result, axis=1)
+     image = Image.open(photo)
+     image = image.convert("L")
+     image = image.resize((48, 48))
 
-    return class_names[int(pred_labels[0])]
+     image_np = np.array(image)
+     image_np = image_np / 255
+
+     input_image = image_np.reshape((1, 48, 48, 1)).astype("float32")
+     result = model.predict(input_image)
+     pred_labels = np.argmax(result, axis=1)
+
+     return class_names[int(pred_labels[0])]
 
 
 def write_text_in_center(text, header):
@@ -35,15 +40,12 @@ write_text_in_center("Please import your file below.", "h3")
 
 
 photo_uploader = st.file_uploader("Upload photo", type=["jpg", "jpeg", "png"])
-write_text_in_center("Or", "h5")
-camera_button = st.button("Take a selfie!", use_container_width=True)
+write_text_in_center("Or take a selfie", "h5")
 
-if camera_button:
-    camera_photo = st.camera_input("Take a picture")
-    st.write(camera_photo is None)
-    if camera_photo is not None:
-        write_text_in_center(evaluate_photo(camera_photo), "h4")
-        # sleep(5)
+camera_photo = st.camera_input("Take a picture")
+st.write(camera_photo is not None)
+if camera_photo is not None:
+	write_text_in_center(evaluate_photo(camera_photo), "h4")
 
 if photo_uploader is not None:
     st.image(photo_uploader, use_column_width="always")
